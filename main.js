8bf36429c03f3b05787ad5551bcc9045f4213223
Okanga <heilbug@Georges-MacBook-Pro.local>
2022-11-02 16:23:01 +0000

    worked on the scan QR modal

---
+++ main.js
@@ -0,0 +1,17 @@
+let qrToggle = document.querySelector(".qr-toggle");
+let qrClose = document.querySelector(".qr-close");
+let overlay = document.getElementById("overlay");
+
+qrToggle.addEventListener("click", function () {
+  document.getElementById("overlay").classList.add("is-visible");
+  document.getElementById("qr-modal").classList.add("is-visible");
+});
+
+qrClose.addEventListener("click", function () {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("qr-modal").classList.remove("is-visible");
+});
+overlay.addEventListener("click", function () {
+  document.getElementById("overlay").classList.remove("is-visible");
+  document.getElementById("qr-modal").classList.remove("is-visible");
+});
